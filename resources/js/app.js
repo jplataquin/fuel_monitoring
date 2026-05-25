377fa65481fc4af9b0e0c67aa77c064c7a8a7ee4
JP Lataquin <jp.lataquin@gmail.com>
2026-05-25 11:44:05 +0800

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
 
 // Alpine is automatically injected by Livewire v3+
-// Removing manual initialization to prevent "multiple instances of Alpine running" warning.
 
 
