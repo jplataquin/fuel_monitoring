a91696baad53537a7e2137d519e6af82cea275f0
JP Lataquin <jp.lataquin@gmail.com>
2026-06-05 00:28:44 +0800

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
+// Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 // Alpine is automatically injected by Livewire v3+
 
 
